332ce10e315e22b67d0b0c58f9f86c3817d464fc
Yutaro <Yutaro@users.noreply.github.com>
2025-06-02 20:45:35 +0900

    詳細画面作成: ルートパラメータ

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -25,4 +25,10 @@ class TodoController extends Controller
         $todo->save(); //オブジェクトの状態をDBに保存(INSERT文)
         return redirect()->route('todo.index');
     }
+    public function show($id)
+    {
+        $model = new Todo();
+        $todo = $model->find($id);
+        return view('todo.show', ['todo' => $todo]);
+    }
 }
