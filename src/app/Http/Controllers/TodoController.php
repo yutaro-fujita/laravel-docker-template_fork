51678e783cc4ec27f68c1ce82ec68e7b1d89fa8f
Yutaro <Yutaro@users.noreply.github.com>
2025-06-02 17:03:35 +0900

    Mass Assignmentとfillableでの入力制限

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -19,10 +19,9 @@ class TodoController extends Controller
     }
     public function store(Request $request)
     {
-        $content = $request->input('content');
+        $inputs = $request->all();
         $todo = new Todo(); //modelのインスタンス化
-        $todo->content = $content; //Todoインスタンスのカラム名(DBに存在するカラム名)のプロパティに値を保存
-        dd($todo); //オブジェクトの状態をDBに保存(INSERT文)
+        $todo->fill($inputs); //今回取得した全key=>valueを埋める
         $todo->save(); //オブジェクトの状態をDBに保存(INSERT文)
         return redirect()->route('todo.index');
     }
