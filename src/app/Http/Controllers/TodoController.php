62f82cc01c0d8d58ecc14aa8e61ec4336c7460cf
Yutaro <Yutaro@users.noreply.github.com>
2025-06-02 16:37:48 +0900

    新規作成処理を実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -17,4 +17,13 @@ class TodoController extends Controller
     {
         return view('todo.create');
     }
+    public function store(Request $request)
+    {
+        $content = $request->input('content');
+        $todo = new Todo(); //modelのインスタンス化
+        $todo->content = $content; //Todoインスタンスのカラム名(DBに存在するカラム名)のプロパティに値を保存
+        dd($todo); //オブジェクトの状態をDBに保存(INSERT文)
+        $todo->save(); //オブジェクトの状態をDBに保存(INSERT文)
+        return redirect()->route('todo.index');
+    }
 }
